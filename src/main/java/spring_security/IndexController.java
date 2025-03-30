8bb9dd67fd90c2a330d86c0cbcdd225f3dfec267
승찬 나 <sk4cks@naver.com>
2025-03-30 21:53:52 +0900

    chore: init

---
+++ src/main/java/spring_security/IndexController.java
@@ -0,0 +1,16 @@
+package spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+}
---
+++ src/test/java/spring_security/SpringSecurityResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
